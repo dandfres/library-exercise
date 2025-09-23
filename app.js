d2e255fd89d9872535783b6f435748b4a8115004
dandfres <168474016+dandfres@users.noreply.github.com>
2025-09-22 23:18:17 -0500

    feat: desktop version implemented

--- app.js
+++ app.js
@@ -3,32 +3,53 @@ const cardGrid = document.querySelector("#card-grid");
 const floatBtns = document.querySelector("#float-btns");
 const Mydialog = document.querySelector("#my-dialog");
 // const cardBook = document.querySelector(".card");
+const modalForm = document.querySelector('form');
 const template = document.querySelector('#template')
 
-// BOOK COLECTION
+// LIBRARY
 const myLibrary = [];
 
 // BOOK CONSTRUCTOR
-function Book(name, auth, pages, read) {
+function Book(name, auth, pages, read, genre) {
     this.name = name;
     this.auth = auth;
     this.pages = pages;
     this.read = read;
+    this.genre = genre;
     this.id = crypto.randomUUID();
 }
 
-// SHOW LAST BOOK
-function renderLibrary() {
-    const lastBook = myLibrary.length - 1;
-    console.log(myLibrary[lastBook]);
-}
-
-function addBookToLibrary(name, auth, pages, read) {
-    const newBook = new Book(name, auth, pages, read);
+// Add Book
+const addBookToLibrary = function (e) {
+    // GET form value
+    const name = e.target.querySelector('#name').value;
+    const auth = e.target.querySelector('#auth').value;
+    const pages = e.target.querySelector('#pages').value;
+    const read = e.target.querySelector('#read-status').dataset.read;
+    const genre = e.target.querySelector('#genre').value;
+    // push myLibrary array
+    const newBook = new Book(name, auth, pages, read, genre);
     myLibrary.push(newBook);
-    renderLibrary();
 }
 
+// SHOW LAST BOOK
+function showLastBook() {
+    const lastBook = myLibrary[myLibrary.length - 1];
+    // clone template
+    const clone = template.content.cloneNode(true);
+    // fill clone content
+    clone.querySelector('.card-name').textContent = lastBook.name;
+    clone.querySelector('.card-auth').textContent = `by ${lastBook.auth}`;
+    clone.querySelector('.pages-value').textContent = lastBook.pages;
+    clone.querySelector('.genre-value').textContent = lastBook.genre;
+    clone.querySelector('.read-status').textContent = lastBook.read;
+    const isRead = lastBook.read === 'Read';
+    clone.querySelector('.read-status').dataset.read = isRead ? 'true' : 'false';
+    // insert card at start
+    cardGrid.prepend(clone);
+}
+
+// Activate edit mode
 function editionMode() {
     const btnEdit = document.querySelector('#btn-edit');
     const btnAdd = document.querySelector('#btn-add');
@@ -40,34 +61,27 @@ function editionMode() {
         btnDelete.dataset.deleteBtn = editMode ? "hidden" : "visible";
         btnEdit.textContent = editMode ? 'Edit' : 'Cancel';
         btnAdd.textContent = editMode ? 'Add +' : 'Save Changes';
-
     })
 }
 
-// Edit and Add listener
+// HANDDLE float buttons
 floatBtns.addEventListener("click", (event) => {
     const button = event.target.closest("button");
     // check that button
     if (button?.matches("#btn-add")) {
         Mydialog.showModal();
         document.activeElement.blur();
-    } 
-    else if (button?.matches("#btn-edit")) editionMode();
+    }
+    else if (button?.matches("#btn-edit")) {
+        editionMode()
+    };
 });
 
-// form button listener
-const modalForm = document.querySelector('form');
-
+// HANDDLE modal form
 modalForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    // get button elements of form
-    getFormAction(e)
-});
-
-const getFormAction = function(e) {
     const button = e.submitter;
-    console.log('button pressed', button.dataset.action)
-
+    // console.log('button pressed', button.dataset.action)
     switch (button.dataset.action) {
         case 'change-reading-status':
             const isActive = button.dataset.read === 'true';
@@ -83,36 +97,17 @@ const getFormAction = function(e) {
             Mydialog.close()
             break;
         case 'save':
-            saveAndShowBook(e);
+            addBookToLibrary(e);
+            showLastBook()
+            Mydialog.close();
+            modalForm.reset();
             break;
     }
-    
-}
+});
 
-
-const saveAndShowBook = function(e) {
-    const name = e.target.querySelector('#name').value;
-    const auth = e.target.querySelector('#auth').value;
-    const pages = e.target.querySelector('#pages').value;
-    const isRead = e.target.querySelector('#read-status').dataset.read;
-    const contentRead = e.target.querySelector('#read-status').textContent;
-
-    const genre = e.target.querySelector('#genre').value;
-    // instantiate and save book
-    const book = new Book(name, auth, pages, isRead, genre);
-    myLibrary.push(book)
-    // clone template
-    const clone = template.content.cloneNode(true);
-    // fill content
-    clone.querySelector('.card-name').textContent = name;
-    clone.querySelector('.card-auth').textContent = `by ${auth}`;
-    clone.querySelector('.pages-value').textContent = pages;
-    clone.querySelector('.genre-value').textContent = genre;
-    clone.querySelector('.read-status').dataset.read = isRead;
-    clone.querySelector('.read-status').textContent = contentRead;
-
-    cardGrid.prepend(clone);
-    Mydialog.close();
-    modalForm.reset();
-}
-
+const book1 = new Book('Game of Thrones', 'George R. R. Martin', '835', 'Not Read', 'fantasy')
+myLibrary.push(book1);
+showLastBook();
+const book2 = new Book('Lord of Rings', 'J.R.R. Tolkien', '1216', 'Read', 'fantasy')
+myLibrary.push(book2);
+showLastBook();
